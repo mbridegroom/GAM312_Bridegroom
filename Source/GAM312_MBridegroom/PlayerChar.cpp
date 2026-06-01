6295e5af4a95bdc7b069940658bd6b4cf78a05c6
Matthew Bridegroom <matthew.bridegroom@snhu.edu>
2026-05-31 23:56:06 -0500

    Module 3 Widget and Building + More

--- Source/GAM312_MBridegroom/PlayerChar.cpp
+++ Source/GAM312_MBridegroom/PlayerChar.cpp
@@ -3,26 +3,26 @@
 
 #include "PlayerChar.h"
 
+
 // Sets default values
 APlayerChar::APlayerChar()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	PlayerCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Cam"));
-
-	PlayerCamComp->SetupAttachment(GetMesh(), "head");
-
+	PlayerCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Cam")); 
+	PlayerCamComp->AttachToComponent(
+		GetMesh(),
+		FAttachmentTransformRules::SnapToTargetNotIncludingScale,
+		TEXT("head")
+	);
 	PlayerCamComp->bUsePawnControlRotation = true;
 
+	BuildingArray.SetNum(3);
 	ResourcesArray.SetNum(3);
 	ResourcesNameArray.Add(TEXT("Wood"));
 	ResourcesNameArray.Add(TEXT("Stone"));
 	ResourcesNameArray.Add(TEXT("Berry"));
-
-
-
-
 
 }
 
@@ -41,6 +41,41 @@ void APlayerChar::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+
+	if (isBuilding)
+	{
+		if (spawnedPart)
+		{
+			FHitResult HitResult;
+
+			// Get camera position
+			FVector StartLocation = PlayerCamComp->GetComponentLocation();
+
+			// Set trace distance
+			FVector Direction = PlayerCamComp->GetForwardVector() * 400.0f;
+			FVector EndLocation = StartLocation + Direction;
+
+			// Setup collision settings
+			FCollisionQueryParams QueryParams;
+			QueryParams.AddIgnoredActor(this);
+			QueryParams.AddIgnoredActor(spawnedPart);
+
+			// Check for placement surface
+			if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
+			{
+				// Place building at hit location
+				spawnedPart->SetActorLocation(HitResult.Location);
+
+			}
+			else
+			{
+				// Place building in front of player
+				spawnedPart->SetActorLocation(EndLocation);
+			}
+
+		}
+	}
+	
 }
 
 // Called to bind functionality to input
@@ -60,19 +95,24 @@ void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &APlayerChar::StopJump);
 
 	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &APlayerChar::FindObject);
+	PlayerInputComponent->BindAction("RotPart", IE_Pressed, this, &APlayerChar::RotateBuilding);
 }
 
 void APlayerChar::MoveForward(float axisValue)
 {
 	// get forward direction from camera rotation
-	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	FRotator Rotation = GetActorRotation();
+
+	FVector Direction = FRotationMatrix(Rotation).GetScaledAxis(EAxis::X);
 	AddMovementInput(Direction, axisValue);
 }
 
 void APlayerChar::MoveRight(float axisValue)
 {
 	// get right direction from camera rotation
-	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	FRotator Rotation = GetActorRotation();
+
+	FVector Direction = FRotationMatrix(Rotation).GetScaledAxis(EAxis::Y);
 	AddMovementInput(Direction, axisValue);
 }
 
@@ -105,66 +145,74 @@ void APlayerChar::FindObject()
 	QueryParams.bTraceComplex = true;
 	QueryParams.bReturnFaceIndex = true;
 
-	// Check for hit object
-	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
+	if (!isBuilding)
 	{
-		// Cast hit actor to resource
-		AResource_M* HitResource = Cast<AResource_M>(HitResult.GetActor());
-
-		// Check stamina amount
-		if (Stamina > 5.0f)
+		// Check for hit object
+		if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
 		{
-			if (HitResource)
+			// Cast hit actor to resource
+			AResource_M* HitResource = Cast<AResource_M>(HitResult.GetActor());
+
+			// Check stamina amount
+			if (Stamina > 5.0f)
 			{
-				FString hitName = HitResource->resourceName;
-				int resourceValue = HitResource->resourceAmount;
-
-				// Remove collected amount
-				HitResource->totalResource = HitResource->totalResource - resourceValue;
-
-				// Resource still available
-				if (HitResource->totalResource > resourceValue)
+				if (HitResource)
 				{
-					GiveResources(resourceValue, hitName);
+					FString hitName = HitResource->resourceName;
+					int resourceValue = HitResource->resourceAmount;
 
-					check(GEngine != nullptr);
-					GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Collected"));
+					// Remove collected amount
+					HitResource->totalResource = HitResource->totalResource - resourceValue;
 
-					// Spawn hit effect
-					UGameplayStatics::SpawnDecalAtLocation(
-						GetWorld(),
-						hitDecal,
-						FVector(10.0f, 10.0f, 10.0f),
-						HitResult.Location,
-						FRotator(-90, 0, 0),
-						2.0f);
+					// Resource still available
+					if (HitResource->totalResource > resourceValue)
+					{
+						GiveResources(resourceValue, hitName);
 
-					// Reduce stamina
-					SetStamina(-5.0f);
-				}
-				else
-				{
-					// Remove empty resource
-					HitResource->Destroy();
+						check(GEngine != nullptr);
+						GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Collected"));
 
-					check(GEngine != nullptr);
-					GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Depleted"));
+						// Spawn hit effect
+						UGameplayStatics::SpawnDecalAtLocation(
+							GetWorld(),
+							hitDecal,
+							FVector(10.0f, 10.0f, 10.0f),
+							HitResult.Location,
+							FRotator(-90, 0, 0),
+							2.0f);
+
+						// Reduce stamina
+						SetStamina(-5.0f);
+					}
+					else
+					{
+						// Remove empty resource
+						HitResource->Destroy();
+
+						check(GEngine != nullptr);
+						GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Depleted"));
+					}
 				}
 			}
-		}
-		else
-		{
-			// Not enough stamina
-			check(GEngine != nullptr);
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Not Enough Stamina"));
+			
 		}
 	}
+	else
+	{
+		isBuilding = false;
+		if (spawnedPart)
+		{
+			spawnedPart->SetActorEnableCollision(true); // FINAL PLACED OBJECT
+			spawnedPart = nullptr;
+		}
+	}
+
 }
 
 void APlayerChar::SetHealth(float Amount)
 {
 	// Keep health below max
-	if (Health + Amount < 100)
+	if (Health + Amount <= 100)
 	{
 		Health = Health + Amount;
 	}
@@ -173,7 +221,7 @@ void APlayerChar::SetHealth(float Amount)
 void APlayerChar::SetHunger(float Amount)
 {
 	// Keep hunger below max
-	if (Hunger + Amount < 100)
+	if (Hunger + Amount <= 100)
 	{
 		Hunger = Hunger + Amount;
 	}
@@ -182,7 +230,7 @@ void APlayerChar::SetHunger(float Amount)
 void APlayerChar::SetStamina(float Amount)
 {
 	// Keep stamina below max
-	if (Stamina + Amount < 100)
+	if (Stamina + Amount <= 100)
 	{
 		Stamina = Stamina + Amount;
 	}
@@ -224,5 +272,70 @@ void APlayerChar::GiveResources(float Amount, FString resourceType)
 	if (resourceType == "Berry")
 	{
 		ResourcesArray[2] = ResourcesArray[2] + Amount;
+	}
+}
+
+void APlayerChar::UpdateResources(float woodAmount, float stoneAmount, FString buildingObject)
+{
+	if (woodAmount <= ResourcesArray[0])
+	{
+		if (stoneAmount <= ResourcesArray[1])
+		{
+			ResourcesArray[0] = ResourcesArray[0] - woodAmount;
+			ResourcesArray[1] = ResourcesArray[1] - stoneAmount;
+
+			if (buildingObject == "Wall")
+			{
+				BuildingArray[0] = BuildingArray[0] + 1;
+			}
+
+			if (buildingObject == "Floor")
+			{
+				BuildingArray[1] = BuildingArray[1] + 1;
+			}
+
+			if (buildingObject == "Ceiling")
+			{
+				BuildingArray[2] = BuildingArray[2] + 1;
+			}
+
+		}
+	}
+}
+
+void APlayerChar::SpawnBuilding(int buildingID, bool& isSuccess)
+{
+	if (!isBuilding)
+	{
+		if (BuildingArray[buildingID] >= 1)
+		{
+			isBuilding = true;
+			FActorSpawnParameters SpawnParams;
+			FVector StartLocation = PlayerCamComp->GetComponentLocation();
+			FVector Direction = PlayerCamComp->GetForwardVector() * 400.0f;
+			FVector EndLocation = StartLocation + Direction;
+			FRotator myRot(0, 0, 0);
+			
+			BuildingArray[buildingID] = BuildingArray[buildingID] - 1;
+
+			spawnedPart = GetWorld()->SpawnActor<ABuildingPart>(BuildingPartClass, EndLocation, myRot, SpawnParams);
+
+			spawnedPart->SetActorEnableCollision(false);
+			spawnedPart->SetActorTickEnabled(true);
+
+			isSuccess = true;
+		
+		}
+
+		isSuccess = false;
+
+	}
+}
+
+void APlayerChar::RotateBuilding()
+{
+	if (isBuilding)
+	{
+		spawnedPart->AddActorLocalRotation(FRotator(0, 90, 0));
 	}
 }
--- Source/GAM312_MBridegroom/PlayerChar.h
+++ Source/GAM312_MBridegroom/PlayerChar.h
@@ -7,6 +7,7 @@
 #include "Camera/CameraComponent.h"
 #include "Resource_M.h"
 #include "Kismet/GameplayStatics.h"
+#include "BuildingPart.h"
 #include "PlayerChar.generated.h"
 
 UCLASS()
@@ -32,81 +33,106 @@ public:
 
 	// moves character forward and backward
 	UFUNCTION()
-	void MoveForward(float axisValue);
+		void MoveForward(float axisValue);
 
 	// moves character left and right
 	UFUNCTION()
-	void MoveRight(float axisValue);
+		void MoveRight(float axisValue);
 
 	// Starts the jump action
 	UFUNCTION()
-	void StartJump();
+		void StartJump();
 
 	// Stops the jump action
 	UFUNCTION()
-	void StopJump();
+		void StopJump();
 
 
 	UFUNCTION()
-	void FindObject();
+		void FindObject();
 
 	// player camera component
 	UPROPERTY(VisibleAnywhere)
-	UCameraComponent* PlayerCamComp;
+		UCameraComponent* PlayerCamComp;
+
 
 	// Player health value
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
-	float Health = 100.0f;
+		float Health = 100.0f;
 
 	// Player hunger value
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
-	float Hunger = 100.0f;
+		float Hunger = 100.0f;
 
 	// Player stamina value
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
-	float Stamina = 100.0f;
+		float Stamina = 100.0f;
 
-	// Total wood amount
+	
 	UPROPERTY(EditAnywhere, Category = "Resources")
-	int Wood;
+		int Wood;
 
-	// Total stone amount
+	
 	UPROPERTY(EditAnywhere, Category = "Resources")
-	int Stone;
+		int Stone;
 
-	// Total berry amount
+	
 	UPROPERTY(EditAnywhere, Category = "Resources")
-	int Berry;
+		int Berry;
 
 	// Stores all resource values
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resources")
-	TArray<int> ResourcesArray;
+		TArray<int> ResourcesArray;
 
 	// Stores resource names
 	UPROPERTY(EditAnywhere, Category = "Resources")
-	TArray<FString> ResourcesNameArray;
+		TArray<FString> ResourcesNameArray;
 
 	// Decal used for hit effect
 	UPROPERTY(EditAnywhere, Category = "HitMarker")
-	UMaterialInterface* hitDecal;
+		UMaterialInterface* hitDecal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building Supplies")
+		TArray<int> BuildingArray;
+
+	UPROPERTY()
+		bool isBuilding;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		TSubclassOf<ABuildingPart> BuildingPartClass;
+
+	UPROPERTY()
+		ABuildingPart* spawnedPart;
+
 
 	// Changes player health
 	UFUNCTION(BlueprintCallable)
-	void SetHealth(float Amount);
+		void SetHealth(float Amount);
 
 	// Changes player hunger
 	UFUNCTION(BlueprintCallable)
-	void SetHunger(float Amount);
+		void SetHunger(float Amount);
 
 	// Changes player stamina
 	UFUNCTION(BlueprintCallable)
-	void SetStamina(float Amount);
+		void SetStamina(float Amount);
 
 	// Updates player stats over time
 	UFUNCTION()
-	void DecreaseStats();
+		void DecreaseStats();
 
 	// Adds resources to inventory
 	UFUNCTION()
-	void GiveResources(float Amount, FString resourceType);
+		void GiveResources(float Amount, FString resourceType);
+
+	UFUNCTION(BlueprintCallable)
+		void UpdateResources(float woodAmount, float stoneAmount, FString buildingObject);
+
+	UFUNCTION(BlueprintCallable)
+		void SpawnBuilding(int buildingID, bool& isSuccess);
+
+	UFUNCTION()
+		void RotateBuilding();
+
+
 };
